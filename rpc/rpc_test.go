952773471bf45f4003416c6a1f8a49fa3104f877
muhammed ikinci <muhammedikinci@outlook.com>
2025-06-24 00:09:35 +0300

    add base message for decode

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -20,13 +20,18 @@ func TestEncode(t *testing.T) {
 }
 
 func TestDecode(t *testing.T) {
-	incomingMessage := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
-	contentLength, err := rpc.DecodeMessage([]byte(incomingMessage))
+	incomingMessage := "Content-Length: 15\r\n\r\n{\"Method\":\"hi\"}"
+	method, content, err := rpc.DecodeMessage([]byte(incomingMessage))
+	contentLength := len(content)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if contentLength != 16 {
-		t.Fatalf("expected: %d, actual: %d", 16, contentLength)
+	if contentLength != 15 {
+		t.Fatalf("expected: %d, actual: %d", 15, contentLength)
+	}
+
+	if method != "hi" {
+		t.Fatalf("expected: %s, actual, %s", "hi", method)
 	}
 }
